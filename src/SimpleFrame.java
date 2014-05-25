42fd211cd2b21d77b6403a9dc8b1c9b1ea6306f1
Dave Gonzalez <dgonzalez@thosegonzos.com>
2014-05-25 14:52:34 -0500

    Initial empty frame.

---
+++ src/SimpleFrame.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class SimpleFrame extends JFrame {
+
+	public SimpleFrame()
+	{
+		super("Frame Title");
+		setSize(300, 100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLookAndFeel();
+		setVisible(true);
+	}
+	
+	private static void setLookAndFeel()
+	{
+		try
+		{
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		}
+		catch(Exception exc)
+		{
+			System.out.println("Error: " + exc.getMessage());
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		setLookAndFeel();
+		SimpleFrame sf = new SimpleFrame();
+	}
+}
